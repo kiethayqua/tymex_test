020d4581ec5b91a34c1c823733cb03571f36b171
kiethayqua <kiethayqua@gmail.com>
2024-12-01 03:10:54 +0700

    chore: remove unused code

--- composeApp/src/commonMain/kotlin/vn/kietnguyendev/tymexhometest/presentation/home/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/vn/kietnguyendev/tymexhometest/presentation/home/HomeScreen.kt
@@ -10,12 +10,8 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
--- composeApp/src/commonMain/kotlin/vn/kietnguyendev/tymexhometest/presentation/navigation/AppNav.kt
+++ composeApp/src/commonMain/kotlin/vn/kietnguyendev/tymexhometest/presentation/navigation/AppNav.kt
@@ -2,39 +2,19 @@ package vn.kietnguyendev.tymexhometest.presentation.navigation
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
 import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.toRoute
-import org.jetbrains.compose.resources.StringResource
-import org.jetbrains.compose.resources.stringResource
-import vn.kietnguyendev.tymexhometest.presentation.component.CustomAppBar
 import vn.kietnguyendev.tymexhometest.presentation.home.HomeRoute
 import vn.kietnguyendev.tymexhometest.presentation.home.HomeScreen
 import vn.kietnguyendev.tymexhometest.presentation.user_detail.UserDetailRoute
 import vn.kietnguyendev.tymexhometest.presentation.user_detail.UserDetailScreen
 import vn.kietnguyendev.tymexhometest.presentation.user_detail.navigateToUserDetail
-import kotlin.reflect.KClass
-
-@Composable
-fun AppBar(
-    title: StringResource,
-    canNavigateBack: Boolean,
-    navigateUp: () -> Unit,
-    modifier: Modifier = Modifier
-) {
-
-}
 
 @Composable
 fun AppNav(navController: NavHostController = rememberNavController()) {
